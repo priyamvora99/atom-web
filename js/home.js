c8644b65b529e03c68e0e12db73eb2cd4e6966fa
Priyam Vora <priyamvora@Priyams-MacBook-Pro.local>
2019-07-21 10:52:51 +0530

    Fixing file upload

--- js/home.js
+++ js/home.js
@@ -19,35 +19,60 @@ $(document).ready(function(){
             alert('Upload attendance in xlsx format');
            }else{
                 //here do computation and upload data to database!
-                 $.ajax({
-                    url:'https://arcane-waters-75160.herokuapp.com/uploadAttendance',
-                    type:'POST',
-                    data:sendObject,
-                    crossDomain:true,
-                    success:(data)=>{
-                        console.log("in sucess");
-                        if(data===true){
-                            $('#acknowledgement').hide();
-                            $('#uploaded').show();
-                            $('#uploadedText').text("Data already uploaded.Head over to ");
-                            $('.summary').html("<a href='../html/summary.html'>Summary Section.</a>");
-                        }else{
-                            $('#uploaded').hide();
-                            $('#acknowledgement').show();
-                            $('#ackText').text(data+"Head over to ");
-                            $('.summary').html("<a href='../html/summary.html'>Summary Section.</a>");
-                        }
+                //  $.ajax({
+                //     url:'https://arcane-waters-75160.herokuapp.com/uploadAttendance',
+                //     type:'POST',
+                //     data:sendObject,
+                //     crossDomain:true,
+                //     success:(data)=>{
+                //         console.log("in sucess");
+                //         if(data===true){
+                //             $('#acknowledgement').hide();
+                //             $('#uploaded').show();
+                //             $('#uploadedText').text("Data already uploaded.Head over to ");
+                //             $('.summary').html("<a href='../html/summary.html'>Summary Section.</a>");
+                //         }else{
+                //             $('#uploaded').hide();
+                //             $('#acknowledgement').show();
+                //             $('#ackText').text(data+"Head over to ");
+                //             $('.summary').html("<a href='../html/summary.html'>Summary Section.</a>");
+                //         }
                         
                         
-                    },error:(err)=>{
-                        console.log("in err");
-                        console.log(err);
-                    }
+                //     },error:(err)=>{
+                //         console.log("in err");
+                //         console.log(err);
+                //     }
 
-                 });
+                //  });
+                if (typeof (FileReader) != "undefined") {
+                    var reader = new FileReader();
+     
+                    //For Browsers other than IE.
+                    if (reader.readAsBinaryString) {
+                        reader.onload = function (e) {
+                            ProcessExcel(e.target.result);
+                        };
+                        reader.readAsBinaryString(fileUpload.files[0]);
+                    } else {
+                        //For IE Browser.
+                        reader.onload = function (e) {
+                            var data = "";
+                            var bytes = new Uint8Array(e.target.result);
+                            for (var i = 0; i < bytes.byteLength; i++) {
+                                data += String.fromCharCode(bytes[i]);
+                            }
+                            ProcessExcel(data);
+                        };
+                        reader.readAsArrayBuffer(fileUpload.files[0]);
+                    }
+                } else {
+                    alert("This browser does not support HTML5.");
+                }
            }
        }
     });
+
     function bs_input_file() {
         $(".input-file").before(
             function() {
@@ -73,6 +98,19 @@ $(document).ready(function(){
                 }
             }
         );
+    }
+    function ProcessExcel(data) {
+        //Read the Excel File data.
+        var workbook = XLSX.read(data, {
+            type: 'binary'
+        });
+ 
+        //Fetch the name of First Sheet.
+        var firstSheet = workbook.SheetNames[0];
+ 
+        //Read all rows from First Sheet into an JSON array.
+        var excelRows = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[firstSheet]);
+        console.log(excelRows);
     }
     
     $(function() {
